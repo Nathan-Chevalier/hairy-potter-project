5162be55563184bdc113f4fff527622fa3a9622d
Nathan Chevalier <cerberusgfx@gmail.com>
2023-07-26 14:40:43 -0500

    firePottery function tested and working correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js";
+import { firePottery } from "./Kiln.js";
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 2, 2);
@@ -8,11 +9,18 @@ let vase = makePottery("Vase", 12, 16);
 let urn = makePottery("Urn", 10, 12);
 let pot = makePottery("Pot", 8, 8);
 
-console.log(mug);
-console.log(bowl);
-console.log(vase);
-console.log(urn);
-console.log(pot);
+let firedPot = firePottery(pot, 2000);
+let firedMug = firePottery(mug, 2000);
+let firedBowl = firePottery(bowl, 2600);
+let firedVase = firePottery(vase, 1800);
+let firedUrn = firePottery(urn, 2000);
+
+console.log(firedPot);
+console.log(firedMug);
+console.log(firedBowl);
+console.log(firedVase);
+console.log(firedUrn);
+
 // Fire each piece of pottery in the kiln
 
 // Determine which ones should be sold, and their price
