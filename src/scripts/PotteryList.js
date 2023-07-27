349185f181cc15fa17fb64c94c80650a850ea5a9
Nathan Chevalier <cerberusgfx@gmail.com>
2023-07-27 09:40:19 -0500

    HTML output passing tests, MVP achieved

--- src/scripts/PotteryList.js
+++ src/scripts/PotteryList.js
@@ -1,5 +1,15 @@
-export const PotteryList = (potterySold) => {
+import { usePottery } from "./PotteryCatalog.js";
+
+export const PotteryList = () => {
   let htmlString = "";
+  let potterySold = usePottery();
   for (const pottery of potterySold) {
+    htmlString += `<section class="pottery" id="pottery--${pottery.id}">
+  <h2 class="pottery__shape">${pottery.shape}</h2>
+  <div class="pottery__properties">
+    Item weighs ${pottery.weight} grams and is ${pottery.height} cm in height
+  </div>
+  <div class="pottery__price">Price is $${pottery.price}</div></section>`;
   }
+  return htmlString;
 };
